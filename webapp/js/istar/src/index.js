78fc43181e89928b8e69c3b274a969225f3b188a
Sotaro Maehara <s.maehara81@icloud.com>
2023-09-09 23:51:56 +0900

    caching

--- webapp/js/istar/src/index.js
+++ webapp/js/istar/src/index.js
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import NodeCache from 'node-cache'
 import axios from 'axios'
 import { Database } from "bun:sqlite";
+
+const nodeCache = new NodeCache({ stdTTL: 60 });
 
 const app = new Hono({
   port: process.env.ISUTAR_PORT || 5001,
@@ -17,9 +20,15 @@ app.use("*", async (ctx, next) => {
 });
 
 const RFC3986URIComponent = (str) => {
-    return encodeURIComponent(str).replace(/[!'()*]/g, (c) => {
+    const ret = nodeCache.get(str);
+    if (ret) {
+      return ret;
+    }
+    const encoded = encodeURIComponent(str).replace(/[!'()*]/g, (c) => {
           return '%' + c.charCodeAt(0).toString(16);
     });
+    nodeCache.set(str, encoded);
+    return encoded;
 };
 
 const dbh = async (ctx) => {
