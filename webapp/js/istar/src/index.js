4480868a957a8500ed22dc1d1dc5d3435cb8175e
Sotaro Maehara <s.maehara81@icloud.com>
2023-09-09 22:59:36 +0900

    use sqlite3

--- webapp/js/istar/src/index.js
+++ webapp/js/istar/src/index.js
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
-import mysql from 'mysql2/promise'
 import axios from 'axios'
+import { Database } from "bun:sqlite";
 
 const app = new Hono({
   port: process.env.ISUTAR_PORT || 5001,
@@ -11,7 +11,7 @@ app.use('*', logger())
 app.use("*", async (ctx, next) => {
   await next();
   if (ctx.dbh) {
-    await ctx.dbh.end();
+    await ctx.dbh.close();
     ctx.dbh = null;
   }
 });
@@ -27,24 +27,24 @@ const dbh = async (ctx) => {
     return ctx.dbh;
   }
 
-  ctx.dbh = await mysql.createConnection({
-    host: process.env.ISUTAR_DB_HOST || 'localhost',
-    port: process.env.ISUTAR_DB_PORT || 3307,
-    user: process.env.ISUTAR_DB_USER || 'isucon',
-    password: process.env.ISUTAR_DB_PASSWORD || 'isucon',
-    database: 'isutar',
-    connectionLimit: 1,
-    charset: 'utf8mb4'
-  });
-  await ctx.dbh.query("SET SESSION sql_mode='TRADITIONAL,NO_AUTO_VALUE_ON_ZERO,ONLY_FULL_GROUP_BY'");
-  await ctx.dbh.query("SET NAMES utf8mb4");
-
+  ctx.dbh = new Database("isutar.db");
   return ctx.dbh;
 }
 
 const initialize = async (ctx) => {
   const db = await dbh(ctx);
-  await db.query('TRUNCATE star');
+  await db.query('DROP TABLE IF EXISTS star');
+  await db.query(`
+    CREATE TABLE IF NOT EXISTS star (
+      id INTGER PRIMARY KEY,
+      keyword VARCHAR(255) NOT NULL,
+      user_name VARCHAR(255) NOT NULL,
+      created_at DATETIME NOT NULL
+    );
+  `)
+  await db.query(`
+    CREATE INDEX keyword_idx ON star (keyword);
+  `);
   return ctx.json({
     result: 'ok',
   })
@@ -53,7 +53,8 @@ const initialize = async (ctx) => {
 const getStars = async (ctx) => {
   const keyword = ctx.req.query("keyword");
   const db = await dbh(ctx);
-  const stars = (await db.query('SELECT keyword, user_name FROM star WHERE keyword = ?', [keyword]))[0];
+  const query = await db.query(`SELECT keyword, user_name FROM star WHERE keyword = $keyword`);
+  const stars = query.all({ $keyword: keyword });
 
   return ctx.json({
     stars,
@@ -74,9 +75,13 @@ const postStars = async (ctx) => {
     return ctx.text("", 404)
   }
 
-  await db.query('INSERT INTO star (keyword, user_name, created_at) VALUES (?, ?, NOW())', [
-    keyword, user
-  ]);
+  const query = db.query(`INSERT INTO star (keyword, user_name, created_at) VALUES ($keyword, $user, "now")`);
+
+  const insert = await db.transaction((velues) => {
+    query.run(velues);
+  })
+
+  const _ = await insert({ $keyword: keyword, $user: user })
 
   return ctx.json({
     result: 'ok',
